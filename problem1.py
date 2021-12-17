1aedf5781d0deedb65bdc1c21b6a558c2dd49e52
Peter Edley <peter@edleyit.com>
2021-12-17 00:33:51 +0000

    start of optomization - removed 29 line of code

--- problem1.py
+++ problem1.py
@@ -1,22 +1,3 @@
-code_key = {
-    0:'0',
-    1:'1',
-    2:'2',
-    3:'3',
-    4:'4',
-    5:'5',
-    6:'6',
-    7:'7',
-    8:'8',
-    9:'9',
-    10:'A',
-    11:'B',
-    12:'C',
-    13:'D',
-    14:'E',
-    15:'F'
-}
-
 version_number_sum = 0
 
 def equal(arrayOfNum):
@@ -28,20 +9,28 @@ def equal(arrayOfNum):
 def lessThan(arrayOfNum):
     if arrayOfNum[0] < arrayOfNum[1]:
         return 1
-    else:
-        return 0
+    return 0
 
 def greaterThan(arrayOfNum):
     if arrayOfNum[0] > arrayOfNum[1]:
         return 1
-    else:
-        return 0
+    return 0
 
 def multiplyList(myList) :
     result = 1
     for x in myList:
          result = result * x
     return result
+
+opperators = {
+    0 : sum,
+    1 : multiplyList,
+    2 : min,
+    3 : max,
+    5 : greaterThan,
+    6 : lessThan,
+    7 : equal 
+}
 
 def parseLiteral(literal):
     last = False
@@ -81,32 +70,16 @@ def parseOperator(packet, type):
             tempStorage.append(result['result'])
             length -= 1
 
-    if type == 0:
-        return {'result':sum(tempStorage), 'usedChars':usedChars}
-    elif type == 1:
-        return {'result':multiplyList(tempStorage), 'usedChars':usedChars}
-    elif type == 2:
-        return {'result':min(tempStorage), 'usedChars':usedChars}
-    elif type == 3:
-        return {'result':max(tempStorage), 'usedChars':usedChars}
-    elif type == 5:
-        return {'result':greaterThan(tempStorage), 'usedChars':usedChars}
-    elif type == 6:
-        return {'result':lessThan(tempStorage), 'usedChars':usedChars}
-    elif type == 7:
-        return {'result':equal(tempStorage), 'usedChars':usedChars}
+    return {'result':opperators[type](tempStorage), 'usedChars':usedChars}
 
 def processPacket(packet):
     global version_number_sum
-
-    if int(packet,2) == 0:
-        return
 
     packet_version = packet[0:3]
     version_number_sum += int(packet_version,2)
     packet_type_ID = packet[3:6]
     usedChars = 6
-    if code_key[int(packet_type_ID,2)] == '4':
+    if int(packet_type_ID,2) == 4:
         result = parseLiteral(packet[usedChars:])
         usedChars += result['usedChars']
         return {'result':result['result'], 'usedChars':usedChars}
@@ -117,13 +90,11 @@ def processPacket(packet):
 
 with open('finaldata.txt') as file:
     for line in file:
-        n=2
-        hexcodes = [line[i:i+n] for i in range(0,len(line.strip()),n)]
+        hexcodes = [line[i:i+2] for i in range(0,len(line.strip()),2)]
 
-num_of_bits = 8
 binary_code = ''
 for hexcode in hexcodes:
-    binary_code += bin(int(hexcode, 16))[2:].zfill(num_of_bits)
+    binary_code += bin(int(hexcode, 16))[2:].zfill(8)
 
 result = processPacket(binary_code)
 
